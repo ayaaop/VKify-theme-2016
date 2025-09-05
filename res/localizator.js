4a9c4a2d948a5cb76474e134890becd9917fc0d6
ayato <58212796+ayaaop@users.noreply.github.com>
2025-09-05 19:26:42 +0500

    make playlist cover image square

--- res/localizator.js
+++ res/localizator.js
@@ -120,7 +120,7 @@ let locales = {
 window.vkifylocalize = function(langcode) {
 	if (!(langcode in locales)) {
 		try {
-		fetch(`/themepack/vkify16/3.3.1.5/resource/langs/${langcode}.json`)
+		fetch(`/themepack/vkify16/3.3.1.6/resource/langs/${langcode}.json`)
 			.then(response => {
 				if (!response.ok) {
 					langcode = 'en';
