37a676b9982708c80aa00507818cfbb35749996e
ayato <myrgdream@gmail.com>
2026-06-22 23:52:45 +0500

    misc style and layout improvements, use avatar.latte in more places

--- res/js/i18n/localizator.js
+++ res/js/i18n/localizator.js
@@ -363,7 +363,7 @@ window.vkifylocalize = function (langcode, fallback = "en") {
 
 	if (!(langcode in locales)) {
 		try {
-			fetch(`/themepack/vkify16/3.3.3.8/resource/langs/${langcode}.json`)
+			fetch(`/themepack/vkify16/3.3.3.9/resource/langs/${langcode}.json`)
 				.then((response) => {
 					if (!response.ok) {
 						patchpage(fallbackCode);
